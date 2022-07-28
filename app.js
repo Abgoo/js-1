c8e29a3b32584c48c5c1c8b4297a91a1824b8991
Gio Abgariani <Gabgariani@gmail.com>
2022-07-28 23:16:19 +0400

    js1

---
+++ app.js
@@ -0,0 +1,73 @@
+// // task 1
+
+const name = "ana";
+let age = 28;
+
+const name1 = "levani";
+let age1 = 21;
+
+let number = age1 - age;
+
+console.log("ana levanze" + number + "wlit ufrosia");
+
+
+// // task 2
+
+const array = ['John', 'Sam', 'Ann', 'Will', 'Andrew', 'Joseph', 'Lorelai', 'Amelie', 'Den'];
+
+let mid = array[4];
+
+console.log(mid);
+
+
+// task 3
+
+let user = {
+    firstName: "Giorgi",
+    lastName: "Totladze",
+    age: 24,
+    subjects: ["math", "sport", "geography", "chemistry", "biology"],
+    roommate: {
+        fullname: "Anri Xalvashi",
+        age: 23
+    }
+}
+
+console.log(user.subjects);
+
+let userFullName = "Giorgi Totladze";
+
+
+
+
+// task 4
+
+// ?????????
+
+let array1 = ["Banana", "Orange", "Apple", "Mango", 2, 12];
+
+while (array1 = "") {
+    console.log(array1.toString)
+}
+
+// ????????
+
+// task 5
+
+var numbers = [12, 23, 43, 11, 9, 2, 121, 90];
+
+if (numbers > 31, numbers % 2) {
+    text = "Element is greater than provided value and is EVEN";
+} else {
+    text = "Element is less than provided value and is ODD"
+}
+
+console.log(text);
+
+// 2nd
+
+if (numbers > 31, numbers % 2) {
+    console.log("Element is greater than provided value and is EVEN");
+} else {
+    console.log("Element is less than provided value and is ODD");
+}
